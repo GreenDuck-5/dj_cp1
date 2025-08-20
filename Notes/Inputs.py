a67597febae1acce381cdc064a1fa83a757d4e47
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-08-20 09:46:52 -0600

    Inputs and Outputs Notes

---
+++ Notes/Inputs.py
@@ -0,0 +1,18 @@
+# DJ, 1st, Input and Outputs Notes
+
+#5 What is an input
+#A way for the code ot get information
+
+#6 What is the purpose of inputs
+#Code isn't helpful if it doesn't use new information
+name = input("What is your name: ")
+# ^      ^          ^
+# save the input where it can be accessed again
+# Use keyword input
+#Inside parentheses give user instructions
+
+print("What is your name")
+name = input()
+
+#Both instruction methods work, but you have to do it the same way all the time
+print("Hello,",name)
