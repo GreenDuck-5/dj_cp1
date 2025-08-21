0f94104627e4aa1ed2c2813dbcd1095c669fe565
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-08-21 09:51:46 -0600

    Variable notes

---
+++ Notes/variables.py
@@ -0,0 +1,13 @@
+# DJ, 1st, Variables Notes
+even_number = 2
+grade = 99
+animal = "Panda"
+print(grade)
+grade = 95
+print(grade)
+print("grade")
+
+
+#name = "green", last_name = "duck"
+#  ^ not good but works
+
