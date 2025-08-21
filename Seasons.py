78a667bae3ccde251126b525b142ef5f9adc3405
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-08-21 09:16:43 -0600

    Seasons

---
+++ Seasons.py
@@ -0,0 +1,6 @@
+#DJ, 1, 8/21 starter - Seasons
+print("What is your last name?")
+last_name = input()
+print("What is your favorite season?")
+season = input()
+print("Wow,",last_name,"san, I like",season,"too!")
