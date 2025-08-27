14fbdc003fa11ae45408e9bfd147b30aa9b59806
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-08-27 09:52:52 -0600

    number notes

---
+++ Notes/numbers.py
@@ -0,0 +1,41 @@
+#DJ, 1st, floats and integers notes
+
+
+#Integers = whole numbers
+age = 999999999999999999999999
+year = 2015
+room = 200
+hour = 9
+motivation = -5
+
+#float = (Floating Point Number)
+pi = 3.14
+percent = -.75
+damage = 9.75
+
+#Math
+print(5+2) #7
+print(5-2) #3
+print(5*2) #10
+print(5/2.5) #2.5
+print(5%2) #1
+print(5**2) #25
+
+print(5+2 *9 +(5**5)%3)
+
+#type casting
+num = int(3.14) #Does not round
+decimal = float(8)
+print(num)
+print(decimal)
+
+#using variables
+hours = 3
+minutes = 4
+
+print(hours*60+minutes)
+
+#To change a user data type you have to surround the ENTIRE input
+age = int(input("How old are you? \n"))
+
+print("Wow you are old, that is", 525600*age, "minutes!")
