c8644589f5e24fb27ebedff31d7516f062368ad9
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-09-03 09:54:14 -0600

    madlibs

---
+++ Projects/madlibs.py
@@ -0,0 +1,31 @@
+#DJ, 1st, Mad Lib
+
+adjective_1 = input("Give me an adjective: ") #1
+
+animal = input("Give me an animal: ") #2 #16
+
+verb_1 = input("Give me a verb, present tense: ") #3
+
+room_in_house = input("Give me a room in the house: ") #4
+
+number = input("Give me a number: ") #5
+
+noun_1 = input("Give me a noun, plural: ") #6
+
+silly_word = input("Give me a silly word: ") #7 #14
+
+body_parts = input("Give me a body part, plural: ") #8
+
+verb_2 = input("Give me a verb, ending in -ing: ") #9
+
+person = input("Give me a famous person: ") #10
+
+color = input("Give me a color: ") #11
+
+liquid = input("Give me a type of liquid: ") #12
+
+emotion = input("Give me an emotion: ") #13
+
+verb_3 = input("Give me a verb, past tense: ") #15
+
+print("Yesterday was a "+adjective_1+" day. I woke up to find "+animal+" had invaded my bathroom and started to "+verb_1+" in the "+room_in_house+". There were exactly "+number+" rubber ducks floating in my tub, each wearing tiny "+noun_1+" and shouting, '"+silly_word+"' at the top of their little "+body_parts+". I tried "+verb_2+", but it only made things worse. One of them looked just like "+person+" but in "+color+" and covered in "+liquid+ ". I was so "+emotion+" I screamed '"+silly_word+"' and ran out of the house. Later, I found out they had "+verb_3+" back to the pond where they came from. And that is why I hate "+animal)
