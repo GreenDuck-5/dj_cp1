a11e6040c04503f885f03b97571cb1a29163c1d0
Dirk <dirk.jones@ucas-edu.net>
2025-09-04 15:23:49 +0000

    string methods

---
+++ Notes/string_methods.py
@@ -0,0 +1,39 @@
+#DJ, 1st, String Methods Notes
+
+subject = "Computer Programming 1!"
+
+print(subject.upper())
+
+print(subject)
+
+
+
+
+# Stupid/Idiot Proofing Inputs
+color = input("What is your favorite color?").strip()
+print("That is cool. I like " + color + " too!")
+
+
+
+
+#What is a method?
+
+
+
+#How do I write a method?
+
+
+
+#What methods can I use to change the formatting of my string?
+
+
+
+#What methods allow me to search the contents of my string?
+
+
+
+#What methods allow me to change the contents of my string?
+
+
+
+#Why is it important to know what type of output a method will give me?
