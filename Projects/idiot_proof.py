069619c8c45a52fb88ca9c08fe7dfac07a6a8154
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-09-05 09:52:46 -0600

    Idiot Proof

---
+++ Projects/idiot_proof.py
@@ -0,0 +1,23 @@
+#DJ, 1st, Idiot Proof
+
+first_name = input("Enter first name: ").title().strip()
+last_name = input("Enter last name: ").title().strip()
+full_name = (first_name,last_name)
+
+
+phone_number = input("Enter phone number: ").strip()
+phone_one , phone_two = phone_number.split()
+
+
+gpa = input("Enter GPA: ")
+
+
+
+
+
+print("Name:" ,  full_name)
+print("Phone Number:",phone_number)
+print()
+
+
+
