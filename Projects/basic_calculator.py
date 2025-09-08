713c84b63106abdbeaae9420e888ded7eb751003
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-09-08 09:53:59 -0600

    basic calculator

---
+++ Projects/basic_calculator.py
@@ -0,0 +1,11 @@
+#DJ, 1st, basic calculator
+
+number_one = int(input("Number 1: "))
+number_two = int(input("Number 2: "))
+
+addition = (number_one + number_two)
+subtraction = (number_one - number_two)
+multiplication = (number_one * number_two)
+division = (number_one / number_two)
+integer_division = (number_one )
+modulo =  (number_one )
