ac929e95549169e11113f1b1fcc4b49c4619b742
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-09-08 09:38:35 -0600

    formatting outputs notes

--- Notes/formatting_outputs.py
+++ Notes/formatting_outputs.py
@@ -5,22 +5,25 @@
 
 name = "Clang"
 age = 21
-money = 25
+money = 100025.1
+percent = .67
 
 
-print("Hello {}. you are {:,}. That is so old! You have ${:.2f} you mast be rich!".format(name, age, money))
+print("Hello {}. you are {:,}. That is so old! You have ${:.2f} you mast be rich! Random percent is {:%}.".format(name, age, money, percent))
 
 
 
 
 # What does the format method allow me to change about my outputs?
-
-
+# (:,) commas where they should be
+# (:E) scientific notation
+# (:b) binary
+# {:. NUmber of decimal places then f}
 
 
 
 # Describe why it is important to format your outputs.
-
+#To make it readable and add context
 
 
 
@@ -29,19 +32,20 @@ print("Hello {}. you are {:,}. That is so old! You have ${:.2f} you mast be rich
 
 # What is an f-string?
 
+#A modern way to format your outputs
 
 
 
 
+# How do I create an f-string?
 
-# How do I create and f-string?
-
+print(f"Hello {name}. you are {age:,}. That is so old! You have ${money:.2f} you mast be rich! Random percent is {percent:%}.")
 
 
 
 
 # What do f-strings allow me to do?
 
-
+#Form outputs in an easy and reliable way
 
 
