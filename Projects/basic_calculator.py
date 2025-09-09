0772e4324ac6e5388a1600d568c9786fe29874f9
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-09-09 09:53:34 -0600

    basic calculator

--- Projects/basic_calculator.py
+++ Projects/basic_calculator.py
@@ -1,22 +1,28 @@
 #DJ, 1st, basic calculator
+running = True
 
-number_one = int(input("Number 1: "))
-number_two = int(input("Number 2: "))
+while running:
+    run = input("Would you like you to run the program?: ").lower().strip()
+    if run == "no":
+        running = False
 
-addition = int((number_one + number_two))
-subtraction = int((number_one - number_two))
-multiplication = int((number_one * number_two))
-division = int((number_one / number_two))
-integer_division = int((number_one // number_two))
-modulo =  int((number_one % number_two))
+    else:
 
-print(f"{number_one} + {number_two} = {addition}")
-print(f"{number_one} - {number_two} = {subtraction}")
-print(f"{number_one} * {number_two} = {multiplication}")
-print(f"{number_one} / {number_two} = {division}")
-print(f"{number_one} // {number_two} = {integer_division}")
-print(f"{number_one} % {number_two} = {modulo}")
+        number_one = int(input("Number 1: ").strip())
+        number_two = int(input("Number 2: ").strip())
 
+        addition = int((number_one + number_two))
+        subtraction = int((number_one - number_two))
+        multiplication = int((number_one * number_two))
+        division = int((number_one / number_two))
+        integer_division = int((number_one // number_two))
+        modulo =  int((number_one % number_two))
 
+        print(f"{number_one} + {number_two} = {addition}")
+        print(f"{number_one} - {number_two} = {subtraction}")
+        print(f"{number_one} * {number_two} = {multiplication}")
+        print(f"{number_one} / {number_two} = {division}")
+        print(f"{number_one} // {number_two} = {integer_division}")
+        print(f"{number_one} % {number_two} = {modulo}")
 
 name = "Clang"
