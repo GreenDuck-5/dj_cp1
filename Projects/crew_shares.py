8f946f3984dbfce14900fa9e322f5e74defefdb3
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-09-10 09:49:02 -0600

    crew shares

---
+++ Projects/crew_shares.py
@@ -0,0 +1,7 @@
+#DJ, 1st, Crew Shares
+
+
+#User inputs for calculations
+total_earned = int(input("How much money did the crew makes?: ").strip())
+crew_members = int(input("How many crew members are there?: ").strip())
+
