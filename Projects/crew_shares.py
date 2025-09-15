bf3d8dad3182dbb3f85bc9c3c8b413ef52c41fac
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-09-15 09:39:52 -0600

    crew shares

--- Projects/crew_shares.py
+++ Projects/crew_shares.py
@@ -2,15 +2,15 @@
 
 
 #User inputs for calculations
-crew_members = int(input(f"How many crew members are there?(Not including the captain and first mate): ").strip())
-total_earned = int(input(f"How much money did the crew makes?: ").strip())
+crew_members = float(input(f"How many crew members are there?(Not including the captain and first mate): ").strip())
+total_earned = float(input(f"How much money did the crew makes?: ").strip())
 
 
-shares = int(total_earned / (crew_members + 10))
+shares = float(total_earned / (crew_members + 10))
 
 
 
 #Print results
-print(f"Each crew member recieves ${shares - 500}")
-print(f"The first mate recieves ${shares * 3}")
-print(f"The captian receives ${shares * 7}")
+print(f"Each crew member recieves ${(shares - 500):.2}")
+print(f"The first mate recieves ${(shares * 3):.2}")
+print(f"The captian receives ${(shares * 7):.2}")
