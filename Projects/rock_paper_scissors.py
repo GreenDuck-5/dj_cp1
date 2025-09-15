f947a894941cdd00819e6ab4e51ed6f23c3fa24f
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-09-15 09:53:00 -0600

    rock paper scissors

---
+++ Projects/rock_paper_scissors.py
@@ -0,0 +1,8 @@
+import random
+
+
+play = input("Would you like to play Rock, Paper, Scissors? Yes or no: ").strip().lower()
+if play == "no"
+    print("ok")
+
+user_choice = input("Rock, Paper, Scissors, Shoot: ").strip().lower()
