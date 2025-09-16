353047cfb3cc4e7515db517df7da11028c21996a
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-09-16 09:05:11 -0600

    rock paper scissors

--- Projects/rock_paper_scissors.py
+++ Projects/rock_paper_scissors.py
@@ -1,8 +1,15 @@
 import random
 
 
-play = input("Would you like to play Rock, Paper, Scissors? Yes or no: ").strip().lower()
-if play == "no"
-    print("ok")
+running = True
 
-user_choice = input("Rock, Paper, Scissors, Shoot: ").strip().lower()
+
+while running:
+    play = input("Would you like to play Rock, Paper, Scissors? Yes or no: ").strip().lower()
+    if play == "no":
+        print("ok")
+        running = False
+
+    else:
+
+        user_choice = input("Rock, Paper, Scissors, Shoot: ").strip().lower()
