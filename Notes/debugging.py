ff7a137df416b9ee96c12aaad5aeec99dd80b1ba
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-09-25 09:54:37 -0600

    debugging notes

---
+++ Notes/debugging.py
@@ -0,0 +1,68 @@
+#DJ, 1st, Debugging notes
+
+
+import time as peter_piper_picked_a_peck_of_purple_pickled_peppers
+import random as how_much_wood_could_a_woodchuck_chuck_if_a_woodchuck_would_chuck_wood
+
+#What is the software development life cycle?
+#A cycle to help plan out your project
+
+#What are the steps in the software development life cycle?
+#1:Project analysis - Figure out details
+#2:Planning phase - Activity diagrams, write out pseudo code, assign tasks
+#3:Execute plan - Write code, debugging
+#4:Test code - More debugging
+#5:Release and maintenance - Even more debugging, updates
+
+#Why do we use the software development life cycle?
+#To only have to do things once
+#To actually solve problems
+#Efficiency
+
+
+#What are the 3 types of bugs that are most likely to occur in a program?
+#Syntax error - grammar error
+# => Read error message, go to that line off code, fix the syntax
+
+#Logic errors - We gave the wrong steps
+
+
+
+#Where do each of these bugs come from?
+
+
+#How do we resolve each type of bug?
+
+
+#What is debugging?
+#The process of removing errors and unintended functions in code
+
+#Why do I need to be comfortable with debugging?
+
+
+
+
+num = 1
+while True:
+    random_number = how_much_wood_could_a_woodchuck_chuck_if_a_woodchuck_would_chuck_wood.randint(1,100)
+    print(random_number)
+    if random_number == 67:
+        print("676767676767676767676767676767")
+        peter_piper_picked_a_peck_of_purple_pickled_peppers.sleep(60)
+    else:
+        print(f"{num}^")
+        peter_piper_picked_a_peck_of_purple_pickled_peppers.sleep(1)
+        num += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
