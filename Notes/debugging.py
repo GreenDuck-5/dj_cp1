dd661db7241b065f39944f169e0155d02f48d4c9
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-09-26 09:34:05 -0600

    debugging notes

--- Notes/debugging.py
+++ Notes/debugging.py
@@ -4,65 +4,81 @@
 import time as peter_piper_picked_a_peck_of_purple_pickled_peppers
 import random as how_much_wood_could_a_woodchuck_chuck_if_a_woodchuck_would_chuck_wood
 
-#What is the software development life cycle?
+# What is the software development life cycle?
 #A cycle to help plan out your project
 
-#What are the steps in the software development life cycle?
+# What are the steps in the software development life cycle?
 #1:Project analysis - Figure out details
 #2:Planning phase - Activity diagrams, write out pseudo code, assign tasks
 #3:Execute plan - Write code, debugging
 #4:Test code - More debugging
 #5:Release and maintenance - Even more debugging, updates
 
-#Why do we use the software development life cycle?
+# Why do we use the software development life cycle?
 #To only have to do things once
 #To actually solve problems
 #Efficiency
 
 
-#What are the 3 types of bugs that are most likely to occur in a program?
+# What are the 3 types of bugs that are most likely to occur in a program?
+#syntax error
+
+#logic error
+
+#run-time error
+while True:
+    denominator = how_much_wood_could_a_woodchuck_chuck_if_a_woodchuck_would_chuck_wood.randint(0,5)
+    if denominator == 0:
+        break
+    else:
+        print(10/denominator)
+    peter_piper_picked_a_peck_of_purple_pickled_peppers.sleep(0)
+
+
+
+# Where do each of these bugs come from?
 #Syntax error - grammar error
 # => Read error message, go to that line off code, fix the syntax
 
 #Logic errors - We gave the wrong steps
+#Go through the logic step by step
 
 
 
-#Where do each of these bugs come from?
+# How do we resolve each type of bug?
+#Rubber Duck Debugging
+#Print Debugging
+#Backtacking debugging
 
 
-#How do we resolve each type of bug?
 
-
-#What is debugging?
+# What is debugging?
 #The process of removing errors and unintended functions in code
 
-#Why do I need to be comfortable with debugging?
+# Why do I need to be comfortable with debugging?
+#Because we do it a lot
+
+
+
 
 
 
 
 num = 1
+six_seven_count = 0
 while True:
-    random_number = how_much_wood_could_a_woodchuck_chuck_if_a_woodchuck_would_chuck_wood.randint(1,100)
+    random_number = how_much_wood_could_a_woodchuck_chuck_if_a_woodchuck_would_chuck_wood.randint(1,1000)
     print(random_number)
     if random_number == 67:
-        print("676767676767676767676767676767")
-        peter_piper_picked_a_peck_of_purple_pickled_peppers.sleep(60)
+        print("6767676767")
+        print(f"{num}^")
+        six_seven_count += 1
+        if six_seven_count == 67:
+            print("You printed 67, 67 times")
+            break
+        else:
+            peter_piper_picked_a_peck_of_purple_pickled_peppers.sleep(0)
+
     else:
         print(f"{num}^")
-        peter_piper_picked_a_peck_of_purple_pickled_peppers.sleep(1)
         num += 1
-
-
-
-
-
-
-
-
-
-
-
-
-
