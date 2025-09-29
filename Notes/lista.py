3f8f56299c7a72914f93a7d4d735c0fc422ff13c
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-09-29 09:13:35 -0600

    lists notes

---
+++ Notes/lista.py
@@ -0,0 +1,52 @@
+#DJ, 1st, Lists notes
+
+
+
+
+
+
+#What are lists? 
+
+
+
+
+#How do you find the length of a list?
+
+
+
+
+#What data types can be put inside of lists?
+
+
+
+
+#How do you get one item from a list?
+
+
+
+
+#How do you replace a value in a list?
+
+
+
+
+#What does append do?
+
+
+
+
+#What does insert do?
+
+
+
+
+#What does extend do?
+
+
+
+
+#How do you remove an item from a list?
+
+
+
+
