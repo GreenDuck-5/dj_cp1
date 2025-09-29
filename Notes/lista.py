bea2d4df8ccb69842d95d44755e585b0f2c11849
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-09-29 09:36:28 -0600

    lists notes

--- Notes/lista.py
+++ Notes/lista.py
@@ -1,52 +1,65 @@
 #DJ, 1st, Lists notes
+import random as peter_piper_picked_a_peck_of_purple_pickled_peppers
 
 
 
+random_color = peter_piper_picked_a_peck_of_purple_pickled_peppers.randint(0,4)
+
+
+colors = ["Green", "Blue", "Yellow", "Purple", "Red"]
+
+print(colors[random_color])
 
 
 
 #What are lists? 
-
+# complex data types used to store data
 
 
 
 #How do you find the length of a list?
-
+print(f"this list is {len(colors)} colors long")
 
 
 
 #What data types can be put inside of lists?
-
+# all
 
 
 
 #How do you get one item from a list?
-
+print(colors[0])
 
 
 
 #How do you replace a value in a list?
 
-
+colors[4] = "Pink"
+colors[3:4] = ["Brown","Maroon"]
 
 
 #What does append do?
-
+colors.append("Orange")
+# Adds a item to the list
 
 
 
 #What does insert do?
-
+colors.insert(2,"Puce")
+# Adds an item where you want it to
 
 
 
 #What does extend do?
-
-
+colors.extend([67,68])
+# Adds items at the end of the list
 
 
 #How do you remove an item from a list?
 
-
-
-
+colors.pop()
+colors.pop(0)
+colors.remove("Maroon")
+print(colors)
+colors.clear()
+print(colors)
