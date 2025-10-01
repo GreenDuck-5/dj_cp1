9348a9b1848740a9e76139206d3a08814078521f
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-10-01 09:18:30 -0600

    fix game and for loops notes

---
+++ Notes/for_loops.py
@@ -0,0 +1,46 @@
+#DJ, 1st, For Loops Notes
+import time as how_much_wood_would_a_woodchuck_chuck_if_a_woodchuck_could_chuck_wood
+import random as peter_piper_picked_a_peck_of_purple_pickled_peppers
+
+
+nums = [4,654,136,84,651,86,42,1,564,3,4894]
+
+
+#for marks beginning of for loop
+for num in nums:
+    num /= 2
+    if num > 100:
+        print(f"{num} is only half of {num*2}. It is a large number")
+        how_much_wood_would_a_woodchuck_chuck_if_a_woodchuck_could_chuck_wood.sleep(1)
+    else:
+        print(num)
+        how_much_wood_would_a_woodchuck_chuck_if_a_woodchuck_could_chuck_wood.sleep(1)
+
+
+
+#What is iteration
+
+
+
+#How do you write a for loop?
+
+
+
+#What is the variable that is represented commonly by i?
+
+
+
+#What can we name that besides i?
+
+
+
+#Why do we indent on the line after the colon?
+
+
+#What is repetition in programming?
+
+
+
+#How do you write a range function?
+
+
