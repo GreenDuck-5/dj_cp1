01d7569f72ed3e6cef4a31ec73a37578374b2411
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-10-06 09:00:48 -0600

    rock paper scissors

--- Practices/rock_paper_scissors.py
+++ Practices/rock_paper_scissors.py
@@ -1,21 +1,18 @@
 #DJ, 1st, Rock paper scissors
 import random
 import time
-tie_count = 0
+tie_count = 0 
 loss_count = 0
 win_count = 0
 while True:
     computer_choice = random.randint(1,3)
     start = input("Would you like you like to play rock paper scissors?:\n").strip().lower()
-    
     if start == "no":
         print("ok bye")
         break 
     else:
         print("Welcome!")
-    
     user_choice = input("Rock, Paper, Scissors, Shoot!:\n").strip().lower()
-    
     if user_choice == "rock" and computer_choice == 1:
         print("Computer chose rock! You tied.")
         tie_count += 1
@@ -49,7 +46,6 @@ while True:
     else:
         print("Please enter valid input.")
         continue
-    
     print(f"You have lost: {loss_count} times\nYou have tied: {tie_count} times\nYou have won: {win_count} times")
     time.sleep(1)
     continue
