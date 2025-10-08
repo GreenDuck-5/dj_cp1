ce892e19c482cba80bd6ef8bdc1a1ef4f5e2fadd
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-10-08 09:42:18 -0600

    pseudocode notes and gamba

---
+++ Notes/pseudocode.py
@@ -0,0 +1,24 @@
+#DJ, 1st, pseudocode notes
+
+#What is pseudocode?
+# A step by step instructions on how to write a program
+
+
+#Why do we write pseudocode? 
+# To have a plan before doing all the hard work in a complicated way
+
+
+#How do you write pseudocode? 
+# Write the code in plain english, just in a planning way
+
+
+#What kinds of words to we swap out for keywords in python? 
+# print > show/display
+
+
+#How can it be helpful to try multiple versions of your pseudocode?
+# To optimize your code
+
+
+
+# when programming with someone else psuedocode is required
--- Other/gamba.py
+++ Other/gamba.py
@@ -1,54 +1,71 @@
 import random as r
 import time as t
 
-def coin_game():
-    print("This is the coin flipping game")
-    t.sleep(0.5)
-    input("How much would you like to gamble?\n")
-    #coin flipping game
-    while True:
-        user_choice = input("Heads or Tails\n").lower().strip()
-        if user_choice == "heads" or "tails":
-            pass
-        else:
-            print("please print valid input")
-            continue
-        print("flipping coin...")
-        t.sleep(1)
-        print("flipping coin...")
-        t.sleep(1)
-        print("flipping coin...")
-        coin = r.randint(1,2)
-        if coin == 1:
-            coin = "heads"
-        elif coin == 2:
-            coin = "tails"
-        t.sleep(1)
-        print(f"The coin is {coin}.")
-        t.sleep(1)
-        if user_choice == coin:
-            print("You win!")
-            print("Round 2")
-            continue
-        else:
-            print("You lose")
-            t.sleep(0.5)
-            print("Goodbye")
-            break
+
 
 
 print("Welcome.")
 t.sleep(1.5)
 print("It's time to play a game.")
 t.sleep(1.5)
-print("I am going to give you 10 tokens")
+print("I am going to give you 10 tokens.")
 t.sleep(1)
 print("+10 Tokens")
 tokens = 10
 t.sleep(1)
-print("Now go.")
-user_input = input("1.) Coin Flipping\n2.) \n3.) \n4.)")
-if user_input == 1:
-    coin_game()
+print("Now go. Go play my game.")
+while True:
+    user_input = input("1.) Coin Flipping\n2.) \n3.) \n4.)\n")
+    if user_input == "1":
+        print("This is the coin flipping game")
+        t.sleep(0.5)
+        while True:
+            wager = int(input("How much would you like to gamble? If you win, you double that much.\n"))
+            if wager > tokens:
+                print(f"You can not wager that much.\n You only have {tokens} tokens.")
+                continue
+            elif wager == 0:
+                print("You cannot wager 0.")
+                continue
+            else:
+                print(f"You are wagering {wager} coins.")
+                break
 
+        while True:
+            user_choice = input("Heads or Tails\n").lower().strip()
+            if user_choice == "heads" or "tails":
+                pass
+            else:
+                print("please print valid input")
+                continue
+
+            print("flipping coin...")
+            t.sleep(1)
+            print("flipping coin...")
+            t.sleep(1)
+            print("flipping coin...")
+            coin = r.randint(1,2)
+            if coin == 1:
+                coin = "heads"
+            elif coin == 2:
+                coin = "tails"
+            t.sleep(1)
+            print(f"The coin is {coin}.")
+            t.sleep(1)
+            if user_choice == coin:
+                print("You win!")
+                t.sleep(0.5)
+                print(f"You won {wager*2} coins")
+                tokens += wager*2
+                break
+            else:
+                print("You lose")
+                t.sleep(0.5)
+                print(f"You lost {wager} coins.")
+                t.sleep(0.5)
+                print("Goodbye")
+                tokens -= wager
+                break
+    else:
+        print("Please enter valid input.")
 
