d207bbf4e59f0ce9b910e2fade7d26fcb2f8826e
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-10-08 09:54:28 -0600

    gamba

--- Other/gamba.py
+++ Other/gamba.py
@@ -1,9 +1,6 @@
 import random as r
 import time as t
 
-
-
-
 print("Welcome.")
 t.sleep(1.5)
 print("It's time to play a game.")
@@ -17,27 +14,28 @@ print("Now go. Go play my game.")
 while True:
     user_input = input("1.) Coin Flipping\n2.) \n3.) \n4.)\n")
     if user_input == "1":
-        print("This is the coin flipping game")
-        t.sleep(0.5)
         while True:
-            wager = int(input("How much would you like to gamble? If you win, you double that much.\n"))
-            if wager > tokens:
-                print(f"You can not wager that much.\n You only have {tokens} tokens.")
-                continue
-            elif wager == 0:
-                print("You cannot wager 0.")
-                continue
-            else:
-                print(f"You are wagering {wager} coins.")
-                break
+            print("This is the coin flipping game")
+            t.sleep(0.5)
+            while True:
+                wager = int(input("How much would you like to gamble? If you win, you double that much.\n"))
+                if wager > tokens:
+                    print(f"You can not wager that much.\n You only have {tokens} tokens.")
+                    continue
+                elif wager == 0:
+                    print("You cannot wager 0.")
+                    continue
+                else:
+                    print(f"You are wagering {wager} coins.")
+                    break
 
-        while True:
-            user_choice = input("Heads or Tails\n").lower().strip()
-            if user_choice == "heads" or "tails":
-                pass
-            else:
-                print("please print valid input")
-                continue
+            while True:
+                user_choice = input("Heads or Tails\n").lower().strip()
+                if user_choice == "heads" or "tails":
+                    break
+                else:
+                    print("please print valid input")
+                    continue
 
             print("flipping coin...")
             t.sleep(1)
@@ -55,17 +53,22 @@ while True:
             if user_choice == coin:
                 print("You win!")
                 t.sleep(0.5)
-                print(f"You won {wager*2} coins")
+                print(f"You won {wager*2} tokens")
                 tokens += wager*2
                 break
             else:
                 print("You lose")
                 t.sleep(0.5)
-                print(f"You lost {wager} coins.")
+                print(f"You lost {wager} tokens.")
                 t.sleep(0.5)
                 print("Goodbye")
                 tokens -= wager
                 break
     else:
         print("Please enter valid input.")
-
+    
+    if tokens <= 0:
+        print("You lose. Goodbye.")
+        print("breaking... \nojfkhgdfsjygfnehsgfdsjgfdimgh\ndisahghdg")
+        break
+        
