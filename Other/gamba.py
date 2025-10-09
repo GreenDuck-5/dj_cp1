ab068569c59d1f9982d9ef8d4f99a952ae332b39
Dirk <dirk.jones@ucas-edu.net>
2025-10-09 02:17:02 +0000

    gamba

--- Other/gamba.py
+++ Other/gamba.py
@@ -2,7 +2,6 @@ import random as r
 import time as t
 while True:
     name = input("Enter name:\n")
-    t.sleep(1)
     print(f"Welcome, {name}.")
     t.sleep(1)
     print("It's time to play a game.")
@@ -15,13 +14,13 @@ while True:
     print("Now go. Go play my game.")
     
     while True:
-        user_input = input("1.) Coin Flipping\n2.) \n3.) \n4.)\n")
+        user_input = input("1.) Coin Flip\n2.) Rock, Paper, Scissors\n3.)\n4.)\n")
         if user_input == "1":
             while True:
                 print("This is the coin flipping game.")
                 t.sleep(0.5)
                 while True:
-                    wager = (input(f"How much would you like to gamble? If you win, you double that much. If you lose, you lose that wager.?\nYou currently have {tokens} tokens.\n"))
+                    wager = (input(f"How much would you like to gamble? If you win, you double that much. If you lose, you lose that wager.\nYou currently have {tokens} tokens.\n"))
                     if wager.isdigit() and int(wager) > tokens:
                         print(f"You can not wager that much.\n You only have {tokens} tokens.")
                         continue
@@ -67,13 +66,70 @@ while True:
                     print(f"You lost {wager} tokens.")
                     print(f"You now have {tokens} tokens.")
                     break
+        if user_input == "2":
+            while True:
+                print("This is rock, paper, scissors.")
+                t.sleep(0.5)
+                while True:
+                    wager = (input(f"How much would you like to gamble? If you win, you double that much. If you lose, you lose that wager. If you tie, you lose nothing and gain nothing.\nYou currently have {tokens} tokens.\n"))
+                    if wager.isdigit() and int(wager) > tokens:
+                        print(f"You can not wager that much.\n You only have {tokens} tokens.")
+                        continue
+                    elif wager.isdigit() and int(wager) == 0:
+                        print("You cannot wager 0.")
+                        continue
+                    elif wager.isdigit() and int(wager):
+                        print(f"You are wagering {wager} coins.")
+                        tokens -= int(wager)
+                        break
+                    else:
+                        print("Please enter a number.")
+                while True:
+                    computer_choice = r.randint(1,3)
+                    user_choice = input("Rock, Paper, Scissors, Shoot!:\n").strip().lower()
+                    if user_choice == "rock" and computer_choice == 1:
+                        print("Computer chose rock! You tied.")
+                        tokens += int(wager)
+                        break
+                    elif user_choice == "rock" and computer_choice == 2:
+                        print("Computer chose paper! You lost.")
+                        break
+                    elif user_choice == "rock" and computer_choice == 3:
+                        print("Computer chose scissors! You win.")
+                        tokens += int(wager)*2
+                        break
+                    elif user_choice == "paper" and computer_choice == 1:
+                        print("Computer chose rock! You win.")
+                        tokens += int(wager)*2
+                        break
+                    elif user_choice == "paper" and computer_choice == 2:
+                        print("Computer chose paper! You tied.")
+                        tokens += int(wager)
+                        break
+                    elif user_choice == "paper" and computer_choice == 3:
+                        print("Computer chose scissors! You lost.")
+                        break
+                    elif user_choice == "scissors" and computer_choice == 1:
+                        print("Computer chose rock! You lost.")
+                        break
+                    elif user_choice == "scissors" and computer_choice == 2:
+                        print("Computer chose paper! You win.")
+                        tokens += int(wager)*2
+                        break
+                    elif user_choice == "scissors" and computer_choice == 3:
+                        print("Computer chose scissors! You tied.")
+                        tokens += int(wager)
+                        break
+                    else:
+                        print("Please enter valid input.")
+                        continue
+                print(f"You now have {tokens} tokens.")
+                break
         else:
             print("Please enter valid input.")
-        
         if tokens <= 0:
             print(f"You lose. Goodbye, {name}.")
             break
         else:
             continue
     continue
-
