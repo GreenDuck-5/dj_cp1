787fa7ae0e003b4f6f2097745cc8b76a29a753bc
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-10-28 09:51:40 -0600

    libraries notes

---
+++ Notes/libraries.py
@@ -0,0 +1,45 @@
+#DJ, 1st, Libraries and Built-n-Functions
+import random as r
+import turtle as T
+import time as t
+#What is a library?
+# a collection of built in functions
+
+#What libraries have we used?
+#random
+#time
+
+#What are some common python libraries?
+#pygame,pybrain,pytorch
+
+#How do you call built-in functions?
+#libraryname.function()
+
+#What are some common functions that we have used?
+r.choice
+r.randint
+r.random
+
+
+
+
+
+colors = ["orange","green","blue","purple","red"]
+#T.fillcolor(r.choice(colors))
+#T.begin_fill()
+#for x in range(1,5):
+    #T.forward(250)
+    #T.right(90)
+#T.end_fill()
+T.shape("turtle")
+T.circle(10)
+T.speed(0)
+while True:
+    for x in range(1,11):
+        for x in range(1,5):
+            T.forward(100)
+            T.right(90)
+        T.right(1)
+    T.forward(15)
+    
+
