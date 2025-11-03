0f0e3d21a39a1051b6b396775a2cbfcb40422428
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-11-03 09:36:08 -0700

    maze

--- Practices/maze_generator.py
+++ Practices/maze_generator.py
@@ -1,9 +1,5 @@
 #DJ, 1st, Maze Generator
-# import libraries to use
-import random
-import time
+#import libraries to use
 import turtle
-# build maze
-    # set maze outline
-    # set background color
-    # 
+import time
+import random
