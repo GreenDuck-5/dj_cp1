d74e84d0cba1d24afd4d9f0a7852995c231a2036
Dirk <dirk.jones@ucas-edu.net>
2025-11-05 00:24:06 +0000

    maze generator

--- Practices/maze_generator.py
+++ Practices/maze_generator.py
@@ -2,35 +2,53 @@
 #import libraries to use
 import turtle
 import random
+#set the turtle speed and making it's shape a turtle because turtles are awesome
 turtle.speed(0)
-turtle.hideturtle()
+turtle.shape("turtle")
+#setting size of cells and grid size, being rows by cols
 cell = 30
 rows = 25
 cols = 25
-turtle.teleport(-375,375)
-turtle.forward(cell*cols)
+#building the edge
+#teleporting to the point to start
+turtle.teleport(-cols*cell/2,rows*cell/2)
+#drawing a line as long as necessary, based off what the row length and cell size it
+turtle.forward(cell*rows)
 turtle.right(90)
-turtle.forward((cell*rows)-cell)
+#giving an enterence/exit
+turtle.forward((cell*cols)-cell)
 turtle.penup()
 turtle.forward(cell)
 turtle.pendown()
 turtle.right(90)
-turtle.forward(cell*cols)
+#same as before
+turtle.forward(cell*rows)
 turtle.right(90)
-turtle.forward((cell*rows)-cell)
+turtle.forward((cell*cols)-cell)
+#setting wall drawing function
 def draw_wall(x, y, direction):
     turtle.penup()
+    #going to cordanates based of the math below in the for loop
     turtle.goto(x, y)
+    #setting angle of turtle movement
     turtle.setheading(direction)
     turtle.pendown()
+    #drawing the wall, moving forward however far "cell" is
     turtle.forward(cell)
     turtle.penup()
+#making the collum generate for every row
 for i in range(rows):
+    #generating collum row
     for j in range(cols):
-        x = j * cell - cols * cell / 2
-        y = i * cell - rows * cell / 2
+        #measuring center of cell then center of screen, centering everything
+        x = j*cell-cols*cell/2
+        y = i*cell-rows*cell / 2
+        #randomly picking whether to run the wall building(1 in 3)
         if random.choice([True, False, False]):
-            draw_wall(x + cell, y, 90)
+            #building a vertrical wall(90)
+            draw_wall(x+cell,y,90)
         if random.choice([True, False, False]):
-            draw_wall(x, y + cell, 0)
+            #building a horizontal wall(0)
+            draw_wall(x,y+cell,0)
+#doesn't end the program till user picks to
 turtle.done()
