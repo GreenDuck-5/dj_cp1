31ae0bc9094eef57021cb19ff246e63ebf84be91
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-11-10 08:59:46 -0700

    dictionaries

---
+++ Notes/dictionaries.py
@@ -0,0 +1,10 @@
+#DJ, 1st, Dictionaries Notes
+
+#What are dictionaries?
+#How do dictionaries simply variables in a project?
+#What are key and value pairs?
+#How do you build a dictionary?
+#How do you call a value from a dictionary?
+#How do you get each of the keys from a dictionary?
+#How do you update a value in a dictionary?
+#Can you add new key and value pairs to a dictionary during runtime?
