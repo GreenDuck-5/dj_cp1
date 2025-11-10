681b251d9a2b92f79ff86785e8ca46aaf5baa010
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-11-10 09:52:59 -0700

    order

---
+++ Practices/order.py
@@ -0,0 +1,18 @@
+#DJ, 1st, Menu
+
+# menu items
+    # mains
+        #item:price
+        #item:price
+        #item:price
+        #etc
+    # drinks
+        #item:price
+        #item:price
+        #etc
+    # sides
+        #item:price
+        #etc
+#ask user what main they want
+    #Loop through the mains, listing them all for user
+    #let them pick one, add to list of user order
