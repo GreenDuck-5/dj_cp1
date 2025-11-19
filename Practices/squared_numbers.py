2a076fd05a7ad9c401e1447aead85b741ca96694
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-11-19 09:44:40 -0700

    squard numbers

---
+++ Practices/squared_numbers.py
@@ -0,0 +1,5 @@
+# DJ, 1st, Squared Numbers
+nums = [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]
+squared = map(lambda num: num*num, nums)
+for i,x in squared:
+    pass
