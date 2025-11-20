c6b75ddf2127c507eea779c8d7eb26345be8e314
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-11-20 09:44:45 -0700

    squard numbers

--- Practices/squared_numbers.py
+++ Practices/squared_numbers.py
@@ -1,3 +1,8 @@
-# DJ, 1st, Squared Numbers
+# DJ, 1st, Squared Numbersn
+
+# loops through numbers 1-20, printing the squared version
+print(list(map(lambda n:n**2,range(1,21))))
+
+
 # loops through numbers 1-20, printing the original number and the squared number
 for n in(range(1,21)):print(n,n**2)
