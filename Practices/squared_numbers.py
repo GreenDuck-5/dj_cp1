2b70cfe239fd31ee5371aca8dd265bf312d16cdb
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-11-20 09:42:48 -0700

    squared numbers

--- Practices/squared_numbers.py
+++ Practices/squared_numbers.py
@@ -1,5 +1,3 @@
 # DJ, 1st, Squared Numbers
-nums = [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]
-squared = map(lambda num: num*num, nums)
-for i,x in squared:
-    pass
+# loops through numbers 1-20, printing the original number and the squared number
+for n in(range(1,21)):print(n,n**2)
