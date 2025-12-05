1a4e36068db06449a5196a67ca9ac08c6c9d36b3
GreenDuck-5 <dirk.jones@ucas-edu.net>
2025-12-05 09:52:15 -0700

    yehaw

---
+++ Projects/wild_west_adventure.py
@@ -0,0 +1,122 @@
+phoenix_brooks = {
+	"Health": 25,
+	"Morale": 10,
+	"Honor": 15,
+	"Money": 100
+}
+
+weapons = {
+    "Damage": {
+        "Fist": 5,
+        "Pistol": 20,
+        "Rifle": 20,
+        "Shotgun": 30,
+        "Boot": 10
+    },
+
+    "Range_m": {
+        "Fist": 1,
+        "Pistol": 45,
+        "Rifle": 1375,
+        "Shotgun": 90,
+        "Boot": 1.5
+    },
+
+    "Accuracy": {
+        "Fist": {
+            "Upper Body": {
+                "Head": 60,
+                "Right Shoulder": 80,
+                "Left Shoulder": 80,
+                "Throat": 50,
+                "Chest": 95,
+                "Stomach": 90
+            },
+            "Lower Body": {
+                "Right Thigh": 40,
+                "Left Thigh": 40,
+                "Right Shin": 20,
+                "Left Shin": 20,
+                "Left Foot": 5,
+                "Right Foot": 5
+            }
+        },
+
+        "Pistol": {
+            "Upper Body": {
+                "Head": 70,
+                "Right Shoulder": 80,
+                "Left Shoulder": 80,
+                "Throat": 45,
+                "Chest": 95,
+                "Stomach": 80
+            },
+            "Lower Body": {
+                "Right Thigh": 20,
+                "Left Thigh": 20,
+                "Right Shin": 10,
+                "Left Shin": 10,
+                "Left Foot": 30,
+                "Right Foot": 30
+            }
+        },
+
+        "Rifle": {
+            "Upper Body": {
+                "Head": 70,
+                "Right Shoulder": 80,
+                "Left Shoulder": 80,
+                "Throat": 45,
+                "Chest": 95,
+                "Stomach": 80
+            },
+            "Lower Body": {
+                "Right Thigh": 20,
+                "Left Thigh": 20,
+                "Right Shin": 10,
+                "Left Shin": 10,
+                "Left Foot": 30,
+                "Right Foot": 30
+            }
+        },
+
+        "Shotgun": {
+            "Upper Body": {
+                "Head": 95,
+                "Right Shoulder": 90,
+                "Left Shoulder": 90,
+                "Throat": 90,
+                "Chest": 95,
+                "Stomach": 90
+            },
+            "Lower Body": {
+                "Right Thigh": 50,
+                "Left Thigh": 50,
+                "Right Shin": 10,
+                "Left Shin": 10,
+                "Left Foot": 30,
+                "Right Foot": 30
+            }
+        },
+
+        "Boot": {
+            "Upper Body": {
+                "Head": 10,
+                "Right Shoulder": 15,
+                "Left Shoulder": 15,
+                "Throat": 10,
+                "Chest": 15,
+                "Stomach": 50
+            },
+            "Lower Body": {
+                "Right Thigh": 70,
+                "Left Thigh": 70,
+                "Right Shin": 90,
+                "Left Shin": 90,
+                "Left Foot": 95,
+                "Right Foot": 95
+            }
+        }
+    }
+}
+
